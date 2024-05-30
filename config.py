3ec46ccf47ac52806a5cd52a252aed01afab0c8b
sezarium <sezare@protonmail.com>
2024-05-30 14:57:45 +0300

    Отслеживание событий target dir

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-source='C:\\Users\\User\\Desktop\\source' # Путь к директории, содержащей задания для FN_RUN
-target='C:\\Users\\User\\Desktop\\target' # Путь к директории, отслеживаемой FN_RUN для выполнения заданий
-file='run.txt' # Имя файла с заданием для FN_RUN
-interval=1     # Пауза между копированием файлов в директорию target, секунд
+source='/Users/dev/Desktop/test/source' # Путь к директории, содержащей задания для FN_RUN
+target='/Users/dev/Desktop/test/target' # Путь к директории, отслеживаемой FN_RUN для выполнения заданий
+task='run.ini' # Имя файла с заданием для FN_RUN
--- start.py
+++ start.py
@@ -1,5 +1,6 @@
-import os, shutil, time
-
+import os, shutil, queue
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 import config
 
 def checkConfigPath(path):
@@ -8,12 +9,37 @@ def checkConfigPath(path):
     print('Директория не существует: ', path)
     return False
 
-targetFilePath = os.path.join(config.target, config.file)
+def executeNextTask():
+    if (not taskQueue.empty()):
+        path = taskQueue.get()
+        print(path, '->', taskFilePath)
+        shutil.copy(path, taskFilePath)
+    else:
+        print('Завершено')
+        observer.stop()
+
+def onFileEvent(event):
+    if (event.src_path == taskFilePath):
+        executeNextTask()
+
+def createEventObserver():
+    handler = FileSystemEventHandler()
+    handler.on_deleted = onFileEvent
+    observer = Observer()
+    observer.schedule(handler, config.target)
+    observer.start()
+    return observer
+
+taskFilePath = os.path.join(config.target, config.task)
+taskQueue = queue.Queue()
 
 if checkConfigPath(config.source) and checkConfigPath(config.target):
     list = os.listdir(config.source)
     for item in list:
         sourceFilePath = os.path.join(config.source, item)
         if os.path.isfile(sourceFilePath):
-            shutil.copy(sourceFilePath, targetFilePath)
-            time.sleep(config.interval)
+            taskQueue.put(sourceFilePath)
+    if (not taskQueue.empty()):
+        observer = createEventObserver()
+        executeNextTask()
+        observer.join()
